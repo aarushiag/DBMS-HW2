36437cf18d85cf82d75d4f444fe75afac25ab85a
Aarushi Agarwal <aarushi16216@iiitd.ac.in>
2018-02-14 18:25:06 +0530

    Add files via upload

--- CCM2.java
+++ CCM2.java
@@ -51,12 +51,12 @@ class Transaction implements Runnable
 	{		
 		if(db.hmap_flight.get(F).hmap_passengers.get(id)==null)
 		{
-				 while( !db.hmap_flight.get(F).flight_lock.tryLock())
+				 while( !db.hmap_flight.get(F).flight_lock.tryLock() || db.hmap_flight.get(F).lock_mode==0)
 				 {
 					 Thread.sleep(1000);
 				 }
 				 
-				 while(!db.hmap_passenger.get(id).pass_lock.tryLock())
+				 while(!db.hmap_passenger.get(id).pass_lock.tryLock() || db.hmap_passenger.get(id).lock_mode==0 )
 				 {
 					 Thread.sleep(1000);
 				 }
@@ -94,14 +94,15 @@ class Transaction implements Runnable
 		Passenger passenger=db.hmap_passenger.get(id);
 		if(flight.hmap_passengers.get(id)!=null)
 		{	
-			while(!db.hmap_flight.get(F).flight_lock.tryLock())
-			{
-				Thread.sleep(1000);
-			}
-			while(!db.hmap_passenger.get(id).pass_lock.tryLock())
-			{
-				Thread.sleep(1000);
-			}
+			while( !db.hmap_flight.get(F).flight_lock.tryLock() || db.hmap_flight.get(F).lock_mode==0)
+			 {
+				 Thread.sleep(1000);
+			 }
+			 
+			 while(!db.hmap_passenger.get(id).pass_lock.tryLock() || db.hmap_passenger.get(id).lock_mode==0 )
+			 {
+				 Thread.sleep(1000);
+			 }
 			
 			db.hmap_flight.get(F).lock_mode=1;
 			db.hmap_passenger.get(id).lock_mode=1;
@@ -131,13 +132,25 @@ class Transaction implements Runnable
 		ArrayList<Flight> arr=new ArrayList<>();
 		for(int i=0;i<db.flights.size();i++)
 		{
-			Flight flight =db.flights.get(i);			 
+			Flight flight =db.flights.get(i);
+			while(flight.lock_mode==1)
+			{
+				
+			}
+			
+			flight.lock_mode=0;			
 			if(flight.hmap_passengers.get(id)!=null)
 			{
 				arr.add(flight);
 			}
-			
 		}
+		
+		for(int i=0;i<db.flights.size();i++)
+		{			
+			Flight flight =db.flights.get(i);
+			flight.lock_mode=-1;
+		}
+		
 		
 		return arr;
 		
@@ -149,7 +162,22 @@ class Transaction implements Runnable
 		
 		for(int u=0; u<db.flights.size(); u++)
 		{
-			sum+=db.flights.get(u).num_res;
+			Flight flight =db.flights.get(u);
+			while(flight.lock_mode==1)
+			{
+				
+			}
+			
+			flight.lock_mode=0;			
+			
+			sum+=flight.num_res;
+		}
+		
+
+		for(int i=0;i<db.flights.size();i++)
+		{			
+			Flight flight =db.flights.get(i);
+			flight.lock_mode=-1;
 		}
 		
 		return sum;
@@ -162,23 +190,43 @@ class Transaction implements Runnable
 		
 		Flight flight1=db.hmap_flight.get(F2);
 		Passenger passenger1=db.hmap_passenger.get(i);
+		
 		if(flight.hmap_passengers.get(i)!=null)
 		{
 			if(F1<F2)
 			{
-				while(!flight.flight_lock.tryLock())
+				while(!flight.flight_lock.tryLock() || flight.lock_mode==0)
+				{
+					Thread.sleep(1000);
+				}				
+				
+				flight.lock_mode=1;
+				
+				while(!flight1.flight_lock.tryLock() || flight1.lock_mode==0)
 				{
 					Thread.sleep(1000);
 				}
-				
-				
-				
-				while(!flight1.flight_lock.tryLock())
-				{
-					Thread.sleep(1000);
-				}
-				
+				flight1.lock_mode=-1;
 							
+			}
+			
+			else
+			{
+				 
+				while(!flight1.flight_lock.tryLock() || flight1.lock_mode==0)
+				{
+					Thread.sleep(1000);
+				}
+				
+				flight1.lock_mode=-1;
+				
+				while(!flight.flight_lock.tryLock() || flight.lock_mode==0)
+				{
+					Thread.sleep(1000);
+				}			
+				
+				flight.lock_mode=1;
+				
 			}
 			
 			flight.hmap_passengers.remove(i);
@@ -186,19 +234,21 @@ class Transaction implements Runnable
 			//System.out.println("Successfully cancelled");
 			
 			if(db.hmap_flight.get(F2).hmap_passengers.get(i)==null)
-			{
-
-					
-					 
-					 
-					 flight1.hmap_passengers.put(i,passenger1);
-					 passenger1.hmap_flights.put(F2, flight1);
-					 
-					 flight1.num_res++;
+			{ 
+				 flight1.hmap_passengers.put(i,passenger1);
+				 passenger1.hmap_flights.put(F2, flight1);
+				 flight1.num_res++;
 			}
 			
 			System.out.println("Successfully transferred");
-			
+			if(F1<F2)
+			{
+				flight1.lock_mode=-1;
+			}
+			else
+			{
+				flight.lock_mode=-1;
+			}
 		}
 		
 		else
@@ -212,17 +262,15 @@ class Transaction implements Runnable
 	public void run() 
 	{	
 		
-		 
-		Random generator = new Random();
-		int random = generator.nextInt(5)+1;
-		int cnt=0;
+		 int cnt=0;
 		
 		do
 		{			
+			System.out.println(Thread.currentThread());
 //			Select a transaction type randomly
 //			Select object(flight and passenger id) for transaction randomly
 //			Invoke transaction
-			generator = new Random();
+			Random generator = new Random();
 			int trans_type = generator.nextInt(5) + 1;
 			
 			if(trans_type == 1)	//reserve
@@ -244,22 +292,34 @@ class Transaction implements Runnable
 				System.out.println("CANCEL");
 				int F = generator.nextInt(num_flights)+1;
 				int id = generator.nextInt(num_passengers)+1;
-				
-				Cancel(F,id);
+	
+				try {
+					Cancel(F,id);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 			
 			else if(trans_type == 3)	//my flights
 			{
 				System.out.println("MY_FLIGHTS");
 				int x=generator.nextInt(num_passengers)+1;
-				ArrayList<Flight> t = My_flight(x);
-				
-				for(int u=0; u<t.size(); u++)
-				{
-					System.out.print("Flight ids for reserved flights for passenger "+x+": ");
-					System.out.print(t.get(u).F+" ");
+				ArrayList<Flight> t;
+				try {
+					t = My_flight(x);
+					for(int u=0; u<t.size(); u++)
+					{
+						System.out.print("Flight ids for reserved flights for passenger "+x+": ");
+						System.out.print(t.get(u).F+" ");
+					}
+					System.out.println();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-				System.out.println();
+				
+				
 			}
 			
 			else if(trans_type == 4)	//total reservations
@@ -275,12 +335,17 @@ class Transaction implements Runnable
 				int F2 = generator.nextInt(num_flights)+1;
 				int id = generator.nextInt(num_passengers)+1;
 			
-				Transfer(F1, F2, id);
+				try {
+					Transfer(F1, F2, id);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 			
 			cnt++;
 			
-		}while(cnt<random);
+		}while(cnt<5);
 		
 		
 	}
@@ -302,8 +367,8 @@ public class CCM2
 		Database obj=new Database();
 			
 		Random generator =new Random();
-		int num_passengers = generator.nextInt(6)+5; //Generate a random number of passengers (5 to 10)
-		int num_flights = generator.nextInt(3)+3; //Generate a random number of passengers (3 to 5)
+		int num_passengers =10; //Generate a random number of passengers (5 to 10)
+		int num_flights =5; //Generate a random number of passengers (3 to 5)
 			
 		for(int j=0; j<num_flights; j++)
 		{
